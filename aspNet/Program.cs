3e045db32e8bef42c121777c0755fffe26d3c397
Dotnet7.0 <zorinos.masterzorin@outlook.com>
2023-08-05 18:18:47 -0300

    asp.net

--- aspNet/Program.cs
+++ aspNet/Program.cs
@@ -1,9 +1,35 @@
+/*
+
+Essas duas linhas de código estão importando os namespaces necessários 
+para usar classes e métodos relacionados à configuração de um aplicativo ASP.NET Core.
+
+*/
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+
+
+/*
+
+Aqui, estamos usando o método estático CreateBuilder(args) da classe WebApplication. 
+Esse método cria uma instância de WebApplicationBuilder, que é usada para construir e 
+configurar um aplicativo web ASP.NET Core. O parâmetro args 
+é uma matriz de argumentos da linha de comando que pode ser usada para configurar o 
+aplicativo durante a inicialização. 
+ 
+*/
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages(); // Suporte para páginas razor
+
+/*
+
+Aqui, estamos usando o método Build() da instância de WebApplicationBuilder para realmente 
+construir o aplicativo. Isso cria e configura a instância do WebApplication que será usada 
+para executar o aplicativo. 
+ 
+*/
 
 var app = builder.Build();
 
@@ -16,4 +42,4 @@ app.UseEndpoints(endpoints => // Configurar as endpoints (rotas)
     endpoints.MapRazorPages(); // Mapeamento de páginas Razor
 });
 
-app.Run(); // Rodar a app
+app.Run(); // Rodar o app
--- aspNet/pages/index.cshtml.cs
+++ aspNet/pages/index.cshtml.cs
@@ -1,3 +1,7 @@
+/*
+ Essas linhas importam os namespaces necessários para usar classes e recursos relacionados a ASP.NET Core MVC e Razor Pages.
+*/
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,10 +11,10 @@ namespace aspNet.pages
     {
         // Renderizado no lado do servidor
         public List<Category> Categories { get; set; } = new();
-        public async Task OnGet() // Requisição Get
+        public async Task OnGet() // Manipulador de requisição HTTP GET
         {
-            await Task.Delay(5000); // Delay para a renderização das informações
-            for(var i =  0; i <= 100; i++)
+            await Task.Delay(5000); // Delay para simular renderização
+            for (var i =  0; i <= 100; i++)
             {
                 Categories.Add(
                     item: new Category(i, 
@@ -27,9 +31,26 @@ namespace aspNet.pages
 
         }
 
-        public void OnPost() // Requisição Post
+        public void OnPost() // Manipulador de requisição HTTP POST
         {
 
         }
     }
 }
+
+/*
+ 
+public List<Category> Categories { get; set; } = new();: Isso declara uma propriedade chamada Categories, 
+que é uma lista de objetos do tipo Category. A lista é inicializada como uma nova lista vazia.
+
+public async Task OnGet(): Este é um método que é executado quando uma solicitação HTTP GET é feita para a 
+página. O código dentro deste método simula uma espera de 5 segundos (usando Task.Delay) e, em seguida, 
+cria 100 objetos Category e os adiciona à lista Categories.
+
+public record Category(int Id, string Title, decimal Price): Isso declara um record chamado Category com três 
+propriedades: Id, Title e Price.
+
+public void OnPost(): Este é um método que é executado quando uma solicitação HTTP POST é feita para a página. 
+No entanto, neste exemplo, o método está vazio e não faz nada.
+ 
+ */
